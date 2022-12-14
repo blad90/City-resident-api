ea06aed9c5f2097baef47a52d10174a55db9feaf
Blad <bladimirbaez@gmail.com>
2022-12-14 15:25:16 -0400

    Initial commit.

---
+++ src/main/java/com/bladbaez/Cityresidentapi/CityResidentApiApplication.java
@@ -0,0 +1,13 @@
+package com.bladbaez.Cityresidentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityResidentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityResidentApiApplication.class, args);
+	}
+
+}
